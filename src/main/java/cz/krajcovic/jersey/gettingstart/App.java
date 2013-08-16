2eff45f812cbc2806f84baedf041aa9dcbb1d46d
Krajcovic <dusan.krajcovic@gmail.com>
2013-08-16 09:14:12 +0200

    Base skelet

---
+++ src/main/java/cz/krajcovic/jersey/gettingstart/App.java
@@ -0,0 +1,13 @@
+package cz.krajcovic.jersey.gettingstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
